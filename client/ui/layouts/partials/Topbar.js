049fc2737c9a9ed65b964c743483bb5d01ede6db
tphien <tranphuochien96@gmail.com>
2017-03-23 21:43:18 +0700

    create topbar and menu

--- client/ui/layouts/partials/Topbar.js
+++ client/ui/layouts/partials/Topbar.js
@@ -1,22 +1,16 @@
 import { ReactiveDict } from 'meteor/reactive-dict';
 import {FlowRouter} from 'meteor/kadira:flow-router';
 
-Template.TopBar.onCreated(function () {
-	
-});
+Template.TopBar.onCreated(function () {});
 
-Template.TopBar.onRendered(function () {
+Template.TopBar.onRendered(function () {});
 
-});
-
-Template.TopBar.onDestroyed(function () {
-
-});
+Template.TopBar.onDestroyed(function () {});
 
 Template.TopBar.helpers({
-    
+    isLogin: function() {
+        return false;
+    }
 });
 
-Template.TopBar.events({
-    
-});
+Template.TopBar.events({});
