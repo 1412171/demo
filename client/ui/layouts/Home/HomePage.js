999a38dfc0df3f0b821cf1e7640bb7c687ceb79e
Han Le Dinh <1412153@student.hcmus.edu.vn>
2017-03-30 22:15:25 +0700

    Config new content

--- client/ui/layouts/Home/HomePage.js
+++ client/ui/layouts/Home/HomePage.js
@@ -76,27 +76,90 @@ Template.HomePage.helpers({
                 BgColor: 'white',
                 Top5: [
                     {
-                        image: '/images/product1.jpg',
+                        image: '/images/Decor/bang_2_mat.jpg',
                         link: '#',
                     },
                     {
-                        image: '/images/product1.jpg',
+                        image: '/images/Decor/bang_ghim_30_40_cm.jpg',
                         link: '#',
                     },
                     {
-                        image: '/images/product1.jpg',
+                        image: '/images/Decor/bang_ghim_40_60_cm.jpg',
                         link: '#',
                     },
                     {
-                        image: '/images/product1.jpg',
+                        image: '/images/Decor/dinh_ghim.jpg',
                         link: '#',
                     },
                     {
-                        image: '/images/product1.jpg',
+                        image: '/images/Decor/flower_flag.jpg',
+                        link: '#',
+                    },
+                    {
+                        image: '/images/Decor/gia_treo_1.jpg',
+                        link: '#',
+                    },
+                    {
+                        image: '/images/Decor/gia_treo_2.jpg',
+                        link: '#',
+                    },
+                    {
+                        image: '/images/Decor/gia_treo_3.jpg',
+                        link: '#',
+                    },
+                    {
+                        image: '/images/Decor/gia_treo_4.jpg',
+                        link: '#',
+                    },
+                    {
+                        image: '/images/Decor/ke_go.jpg',
+                        link: '#',
+                    },
+                    {
+                        image: '/images/Decor/kep_go.jpg',
+                        link: '#',
+                    },
+                    {
+                        image: '/images/Decor/kep_go_6_loai.jpg',
+                        link: '#',
+                    },
+                    {
+                        image: '/images/Decor/kep_go_cau_vong.jpg',
+                        link: '#',
+                    },
+                    {
+                        image: '/images/Decor/khung_khong_lo.jpg',
+                        link: '#',
+                    },
+                    {
+                        image: '/images/Decor/khung_map.jpg',
+                        link: '#',
+                    },
+                    {
+                        image: '/images/Decor/khung_om.jpg',
+                        link: '#',
+                    },
+                    {
+                        image: '/images/Decor/khung_to_bu.jpg',
+                        link: '#',
+                    },
+                    {
+                        image: '/images/Decor/lucy.jpg',
+                        link: '#',
+                    },
+                    {
+                        image: '/images/Decor/party_flag_set.jpg',
+                        link: '#',
+                    },
+                    {
+                        image: '/images/Decor/rainy_set.jpg',
+                        link: '#',
+                    },
+                    {
+                        image: '/images/Decor/travel_flag.jpg',
                         link: '#',
                     }
                 ]
-
             },
             {
                 Name: "LIGHT",
@@ -104,23 +167,27 @@ Template.HomePage.helpers({
                 BgColor: '#efefef',
                 Top5: [
                     {
-                        image: '/images/product1.jpg',
+                        image: '/images/Lights/ball_bling.jpg',
                         link: '#',
                     },
                     {
-                        image: '/images/product1.jpg',
+                        image: '/images/Lights/bling_bling.jpg',
                         link: '#',
                     },
                     {
-                        image: '/images/product1.jpg',
+                        image: '/images/Lights/chips_bling.jpg',
                         link: '#',
                     },
                     {
-                        image: '/images/product1.jpg',
+                        image: '/images/Lights/letter_bling.jpg',
                         link: '#',
                     },
                     {
-                        image: '/images/product1.jpg',
+                        image: '/images/Lights/star_bling.jpg',
+                        link: '#',
+                    },
+                    {
+                        image: '/images/Lights/tiny_bling.jpg',
                         link: '#',
                     }
                 ]
@@ -132,27 +199,18 @@ Template.HomePage.helpers({
                 BgColor: 'white',
                 Top5: [
                     {
-                        image: '/images/product1.jpg',
+                        image: '/images/Notebooks/lucky_planer.jpg',
                         link: '#',
                     },
                     {
-                        image: '/images/product1.jpg',
+                        image: '/images/Notebooks/note_for.jpg',
                         link: '#',
                     },
                     {
-                        image: '/images/product1.jpg',
-                        link: '#',
-                    },
-                    {
-                        image: '/images/product1.jpg',
-                        link: '#',
-                    },
-                    {
-                        image: '/images/product1.jpg',
+                        image: '/images/Notebooks/tui_chong_tham_lucky_planer.jpg',
                         link: '#',
                     }
                 ]
-
             },
         ]
     }
