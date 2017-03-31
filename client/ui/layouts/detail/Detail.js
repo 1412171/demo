b31f54966570f5618d680a585dbb4d9769219016
tphien <tranphuochien96@gmail.com>
2017-03-31 21:46:20 +0700

    introduce modal bootstrap for Quang

---
+++ client/ui/layouts/detail/Detail.js
@@ -0,0 +1,22 @@
+/**
+ * Created by OP on 3/31/2017.
+ */
+Template.Detail.onCreated(function () {
+});
+
+Template.Detail.onRendered(function () {
+    //Xử lý khởi tạo slider
+});
+
+Template.Detail.onDestroyed(function () {
+});
+
+Template.Detail.helpers({
+});
+
+Template.Detail.events({
+    'click #main-pic':function (event) {
+        console.log("vsdvds");
+        // xử lý code khi click vào tấm ảnh (nếu cần)
+    }
+});
