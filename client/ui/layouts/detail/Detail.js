a39b5c7e1249d17eaaa3a029f0d493f1f7089ad9
dangquang2509 <lidaqua@gmail.com>
2017-04-02 23:23:50 +0700

    update slider

--- client/ui/layouts/detail/Detail.js
+++ client/ui/layouts/detail/Detail.js
@@ -1,22 +1,58 @@
 /**
- * Created by OP on 3/31/2017.
+ * Created by Administrator on 31/03/2017.
  */
+import './Detail.html'
 Template.Detail.onCreated(function () {
 });
 
-Template.Detail.onRendered(function () {
-    //Xử lý khởi tạo slider
+Template.Detail.onDestroyed(function () {
 });
 
-Template.Detail.onDestroyed(function () {
+Template.Detail.onRendered(function () {
+    Template.instance().$('.slider-for2').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        pauseOnHover:true,
+        asNavFor: '.slider-nav2'
+    });
+    Template.instance().$('.slider-nav2').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for2',
+        dots: true,
+        centerMode: true,
+        speed:1000,
+        focusOnSelect: true
+    });
+    Template.instance().$('.slider-for1').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        pauseOnHover:true,
+        asNavFor: '.slider-nav1'
+    });
+    Template.instance().$('.slider-nav1').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for1',
+        dots: true,
+        centerMode: true,
+        speed:1000,
+        focusOnSelect: true
+    });
 });
 
 Template.Detail.helpers({
 });
 
 Template.Detail.events({
-    'click #main-pic':function (event) {
-        console.log("vsdvds");
-        // xử lý code khi click vào tấm ảnh (nếu cần)
-    }
+
 });
+
+
+
+
+
